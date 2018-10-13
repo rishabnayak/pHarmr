a2328473e295dcb08a7aa33fb7bc08b97c4c48c5
rishab2113 <rishab@bu.edu>
2018-10-13 08:40:56 -0400

    updates

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,20 +1,23 @@
 <template>
-  <main role="main">
-    <section class="jumbotron text-center">
-        <div class="container">
-          <h1 class="jumbotron-heading">pHarmr</h1>
-          <p class="lead text-muted">Automate your Hydroponics with pHarmr</p>
-        </div>
-      </section>
+<main role="main">
+  <section class="jumbotron text-center">
+    <div class="container">
+      <h1 class="jumbotron-heading">pHarmr</h1>
+      <p class="lead text-muted">Automate your Hydroponics with pHarmr</p>
+      <p>{{data}}</p>
+    </div>
+  </section>
 
 
-            <hr class="featurette-divider">
+  <hr class="featurette-divider">
 
-          <footer class="container">
-            <p class="float-right"><a href="#">Back to top</a></p>
-            <p>© 2018 pHarmr · <a><router-link :to="{ name: 'privacy'}">Privacy</router-link></a></p>
-          </footer>
-    </main>
+  <footer class="container">
+    <p class="float-right"><a href="#">Back to top</a></p>
+    <p>© 2018 pHarmr · <a>
+        <router-link :to="{ name: 'privacy'}">Privacy</router-link>
+      </a></p>
+  </footer>
+</main>
 </template>
 <script>
 import firebase from 'firebase'
@@ -23,22 +26,28 @@ import db from '@/firebase/init.js'
 export default {
   name: 'home',
   computed: {
-    user () {
+    user() {
       return this.$store.state.user
     }
   },
-async mounted (){
-  if (this.user) {
-    if (this.user.uname == undefined) {
-      this.$router.push({ name: "editprofile"})
-    }
-    else {
+  methods: {
+
+  },
+  async mounted() {
+    if (this.user) {
+      if (this.user.uname == undefined) {
+        this.$router.push({
+          name: "editprofile"
+        })
+      } else {
+        return
+      }
+    } else {
       return
     }
-  }
-  else {
-    return
-    }
+  },
+  async created() {
+
   }
 }
 </script>
