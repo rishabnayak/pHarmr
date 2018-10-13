85b29a1e8a7479c583061d97404abe8965a13dfd
rishab2113 <rishab@bu.edu>
2018-10-13 18:53:51 -0400

    logo

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,22 +1,20 @@
 <template>
 <main role="main">
-  <section class="jumbotron text-center">
-    <div class="container">
-      <h1 class="jumbotron-heading">pHarmr</h1>
-      <p class="lead text-muted">Automate your Hydroponics with pHarmr</p>
-      <button class="btn btn-primary btn-lg btn-block col-md-3" @click="doSomething()">Click</button>
-    </div>
-  </section>
+  <section class="jumbotron text-center">
+    <div class="container">
+      <img class="logo" src="@/assets/pHarmr_logo.png"/>
+      <p class="lead text-muted">Automate your Hydroponics with pHarmr</p>
+    </div>
+  </section>
 
+  <hr class="featurette-divider">
 
-  <hr class="featurette-divider">
-
-  <footer class="container">
-    <p class="float-right"><a href="#">Back to top</a></p>
-    <p>© 2018 pHarmr · <a>
-        <router-link :to="{ name: 'privacy'}">Privacy</router-link>
-      </a></p>
-  </footer>
+  <footer class="container">
+    <p class="float-right"><a href="#">Back to top</a></p>
+    <p>© 2018 pHarmr · <a>
+        <router-link :to="{ name: 'privacy'}">Privacy</router-link>
+      </a></p>
+  </footer>
 </main>
 </template>
 <script>
@@ -24,33 +22,39 @@ import firebase from 'firebase'
 import firebaseui from 'firebaseui'
 import db from '@/firebase/init.js'
 export default {
-  name: 'home',
-  computed: {
-    user() {
-      return this.$store.state.user
-    }
-  },
-  methods: {
-    doSomething(){
-      var xhr = new XMLHttpRequest()
-      xhr.open("GET", "https://pubsub.googleapis.com/v1/projects/pharmr-2018/topics/dht11",true)
-      console.log(xhr.send())
-    }
-  },
-  async mounted() {
-    if (this.user) {
-      if (this.user.uname == undefined) {
-        this.$router.push({
-          name: "editprofile"
-        })
-      } else {
-        return
-      }
-    } else {
-      return
-    }
-  },
-  async created() {
-  }
+  name: 'home',
+  computed: {
+    user() {
+      return this.$store.state.user
+    }
+  },
+  methods: {
+    doSomething(){
+      var xhr = new XMLHttpRequest()
+      xhr.open("GET", "https://pubsub.googleapis.com/v1/projects/pharmr-2018/topics/dht11",true)
+      console.log(xhr.send())
+    }
+  },
+  async mounted() {
+    if (this.user) {
+      if (this.user.uname == undefined) {
+        this.$router.push({
+          name: "editprofile"
+        })
+      } else {
+        return
+      }
+    } else {
+      return
+    }
+  },
+  async created() {
+  }
 }
 </script>
+<style>
+.logo {
+    max-width: 600px;
+    max-height: 300px;
+}
+</style>
